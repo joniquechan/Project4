adbf5f4b654df4425fda4a8a219798d49997f1b6
joniquechan <uniquejoniqueC@gmail.com>
2024-11-05 22:14:10 -0500

    first commit

---
+++ src/main/java/com/mycompany/project4algo/Project4Algo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project4algo;
+
+/**
+ *
+ * @author User
+ */
+public class Project4Algo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
